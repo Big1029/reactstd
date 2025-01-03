a4a4e6b0b4492e45e536399a8a0d0a7ccb26a214
Big1029 <d2ryu01@naver.com>
2025-01-03 15:46:47 +0900

    긍정,기본, 부정버튼 생성후 디자인

--- src/component/Button.js
+++ src/component/Button.js
@@ -2,6 +2,7 @@ import "./Button.css"
 
 const Button = ({ text, type, onClick }) => {
   const btnType = ["positive", "negative"].includes(type) ? type : "default"
+  console.log(type)
   return (
     <button
       className={["Button", `Button_${btnType}`].join(" ")}
@@ -10,5 +11,8 @@ const Button = ({ text, type, onClick }) => {
       {text}
     </button>
   )
+}
+Button.dedaultProps = {
+  type: "default"
 }
 export default Button
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,26 @@ const Home = () => {
   return (
     <div>
       <Button
-        text={"버튼 텍스트"}
+        type="default"
+        text={"기본 버튼튼"}
         onClick={() => {
-          alert("hi")
+          alert("default button")
+        }}
+      />
+
+      <Button
+        type="positive"
+        text={"긍정 버튼튼"}
+        onClick={() => {
+          alert("positive button")
+        }}
+      />
+
+      <Button
+        type="negative"
+        text={"부정 버튼"}
+        onClick={() => {
+          alert("negative button")
         }}
       />
     </div>
