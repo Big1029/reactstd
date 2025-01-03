5194cd6eb3da4c2ca379e26420fbca6e8bd178a2
Big1029 <d2ryu01@naver.com>
2025-01-03 15:25:30 +0900

    Button 컴포넌트 추가 및 페이지 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,15 @@
+import Button from "../component/Button";
+
 const Home = () => {
-  return <div>Home 페이지입니다</div>
+  return (
+    <div>
+      <Button
+        text={"버튼 텍스트"}
+        onClick={() => {
+          alert("hi")
+        }}
+      />
+    </div>
+  )
 }
 export default Home
